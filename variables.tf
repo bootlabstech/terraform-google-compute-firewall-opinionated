aa55c6862123f710c96aabea2cf8aff120520832
Silpashree1 <94901964+Silpashree1@users.noreply.github.com>
2022-02-16 16:39:21 +0530

    updated variables.tf for auto-complete.

--- variables.tf
+++ variables.tf
@@ -23,7 +23,18 @@ variable "project" {
 
 variable "direction" {
   type        = string
-  description = "Direction of traffic to which this firewall applies; default is INGRESS. Note: For INGRESS traffic, it is NOT supported to specify destinationRanges; For EGRESS traffic, it is NOT supported to specify source_ranges OR source_tags. For INGRESS traffic, one of source_ranges, source_tags or source_service_accounts is required. Possible values are INGRESS and EGRESS."
+  description =  <<-EOT
+  {
+   "type": "json",
+   "purpose": "autocomplete",
+   "data": [
+  "INGRESS",
+  "EGRESS"
+   ],
+   "default":"INGRESS"
+   "description": "Direction of traffic to which this firewall applies."
+}
+EOT 
 }
 
 variable "source_ranges" {
